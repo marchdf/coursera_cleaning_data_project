364c53e78f1e8a20fdce902aa4d71ebc2ca4dcc4
Marc Henry de Frahan (lab) <marchdf@umich.edu>
2015-12-17 16:25:31 -0500

    Starting to load/merge the data.

---
+++ run_analysis.R
@@ -0,0 +1,42 @@
+run_analysis <- function(){
+
+    ## Setup variables
+    datadir <- "UCI_HAR_Dataset/"
+
+    ## load the feature names
+    features <- read.table(paste0(datadir,"features.txt"),col.names=c("index","name"))
+    
+    ## load the data and name the columns as we load them up
+    x_test <- read.table(paste0(datadir,"test/X_test.txt"),col.names=features$name)
+    y_test <- read.table(paste0(datadir,"test/y_test.txt"),col.names="activity")
+    subject_test <- read.table(paste0(datadir,"test/subject_test.txt"),col.names="subject")
+    x_train <- read.table(paste0(datadir,"train/X_train.txt"),col.names=features$name)
+    y_train <- read.table(paste0(datadir,"train/y_train.txt"),col.names="activity")
+    subject_train <- read.table(paste0(datadir,"train/subject_train.txt"),col.names="subject")
+    
+    ## Merge the data so that it now looks like:
+    ## s | y | 
+    ## t | t | 
+    ## e | e | xtest 
+    ## s | s |  
+    ## t | t | 
+    ##   |   |  
+    ## s | y | 
+    ## t | t | 
+    ## r | r | xtrain
+    ## a | a | 
+    ## i | i | 
+    ## n | n |  
+    df <- rbind(cbind(subject_test,y_test,x_test),cbind(subject_train,y_train,x_train))
+
+    
+    print(str(x_test))
+    print(str(y_test))
+    print(str(subject_test))
+    print(str(x_train))
+    print(str(y_train))
+    print(str(subject_train))
+
+
+
+}
