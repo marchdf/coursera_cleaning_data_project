b0793e5914d08c7daf206c5faa6e843ec2a07804
Marc Henry de Frahan (lab) <marchdf@umich.edu>
2015-12-17 16:54:54 -0500

    Renaming the activities.

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,13 @@
 run_analysis <- function(){
 
+    library(plyr)
+    
     ## Setup variables
     datadir <- "UCI_HAR_Dataset/"
 
     ## load the feature names
     features <- read.table(paste0(datadir,"features.txt"),col.names=c("index","name"))
+    activities <- read.table(paste0(datadir,"activity_labels.txt"),col.names=c("index","name"))
     
     ## load the data and name the columns as we load them up
     x_test <- read.table(paste0(datadir,"test/X_test.txt"),col.names=features$name)
@@ -29,14 +32,9 @@ run_analysis <- function(){
     ## n | n |  
     df <- rbind(cbind(subject_test,y_test,x_test),cbind(subject_train,y_train,x_train))
 
-    
-    print(str(x_test))
-    print(str(y_test))
-    print(str(subject_test))
-    print(str(x_train))
-    print(str(y_train))
-    print(str(subject_train))
 
+    ## Rename the activities indices to their full name
+    df$activity <- mapvalues(df$activity,from=activities$index,to=as.character(activities$name))
 
 
 }
